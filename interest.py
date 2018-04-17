adad3fe00af82e0e73005fbdb61a749d2c778af2
jpolakowski <johnpolakowski@hotmail.com>
2018-04-16 21:18:47 -0700

    my commit to this repo

---
+++ interest.py
@@ -0,0 +1,7 @@
+interest_percent = .025
+money_invested = 2130000
+total_interest_earned = money_invested * interest_percent
+
+print("The initial investment amount is: {}".format(money_invested))
+print("Enter the interest per year: {}%".format(interest_percent))
+print("The total interest earned will be {}".format(total_interest_earned))
